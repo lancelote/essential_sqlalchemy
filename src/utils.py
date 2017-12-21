836676deb8a7e39758cc3a920cecc71ab6c78d83
Pavel Karateev <karateev.pavel@ya.ru>
2017-12-21 16:11:19 +0300

    Updates create_sqlite_db util
    Now it'll create directory to store databases if it does't exist

--- src/utils.py
+++ src/utils.py
@@ -6,5 +6,8 @@ ROOT = os.path.join(os.path.dirname(__file__), '..')
 
 def create_sqlite_db(name: str):
     """Create sqlite3 database inside dbs folder."""
-    connection = sqlite3.connect(f'{ROOT}/dbs/{name}')
+    folder = os.path.join(ROOT, 'dbs')
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    connection = sqlite3.connect(f'{folder}/{name}')
     connection.close()
