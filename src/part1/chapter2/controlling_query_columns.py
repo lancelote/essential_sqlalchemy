4fb52a9a6ef3c22459629f5edbbb30ddffe81a38
Pavel Karateev <karateev.pavel@ya.ru>
2017-12-22 18:20:18 +0300

    Controlling query columns example

---
+++ src/part1/chapter2/controlling_query_columns.py
@@ -0,0 +1,10 @@
+from sqlalchemy import select
+
+from src.part1.chapter2.result_proxy import cookies, connection
+
+selection = select([cookies.c.cookie_name, cookies.c.quantity])
+result_proxy = connection.execute(selection)
+assert result_proxy.keys() == ['cookie_name', 'quantity']  # List of columns
+
+result = result_proxy.first()
+assert result == ('chocolate chip', 12)
