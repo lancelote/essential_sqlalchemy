80b4eeff59b9b1eda4a29fd946e3b528edbaa9aa
Pavel Karateev <karateev.pavel@ya.ru>
2017-12-26 15:12:32 +0300

    Fixes typo in ordering example

--- src/part1/chapter2/ordering.py
+++ src/part1/chapter2/ordering.py
@@ -1,10 +1,14 @@
-from sqlalchemy import select
+from sqlalchemy import select, desc
 
 from src.part1.chapter2.controlling_query_columns import cookies, connection
 
 selection = select([cookies.c.cookie_name, cookies.c.quantity])
-selection = selection.order_by(cookies)
+selection = selection.order_by(desc(cookies.c.quantity))
 result_proxy = connection.execute(selection)
+
 assert result_proxy.fetchall() == [
-    ('chocolate chip', 12), ('dark chocolate chip', 1),
-    ('peanut butter', 24), ('oatmeal raisin', 100)]
+    ('oatmeal raisin', 100),
+    ('peanut butter', 24),
+    ('chocolate chip', 12),
+    ('dark chocolate chip', 1)
+]
