a29e93c7d3d7a885ff154dba788b5e0c6ab299e1
Pavel Karateev <karateev.pavel@ya.ru>
2018-01-03 17:06:39 +0300

    Deleting data example

---
+++ src/part1/chapter2/deleting.py
@@ -0,0 +1,11 @@
+from sqlalchemy import delete, select
+
+from src.part1.chapter2.updating import cookies, connection
+
+selection = delete(cookies).where(cookies.c.cookie_name == 'dark chocolate chip')
+result_proxy = connection.execute(selection)
+assert result_proxy.rowcount == 1
+
+selection = select([cookies]).where(cookies.c.cookie_name == 'dark chocolate chip')
+result_proxy = connection.execute(selection).fetchall()
+assert len(result_proxy) == 0  # Data was removed
