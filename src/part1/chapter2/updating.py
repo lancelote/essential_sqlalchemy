d902b1487b93e95ba65342adfdb9aae7063536e9
Pavel Karateev <karateev.pavel@ya.ru>
2018-01-03 17:02:02 +0300

    Updating data example

---
+++ src/part1/chapter2/updating.py
@@ -0,0 +1,20 @@
+from sqlalchemy import update, select
+
+from src.part1.chapter2.conjunctions import cookies, connection
+
+selection = update(cookies).where(cookies.c.cookie_name == 'chocolate chip')
+selection = selection.values(quantity=(cookies.c.quantity + 120))
+result_proxy = connection.execute(selection)
+assert result_proxy.rowcount == 1
+
+selection = select([cookies]).where(cookies.c.cookie_name == 'chocolate chip')
+result_proxy = connection.execute(selection).first()
+# for key in result_proxy.keys():
+#     print('{:>20}: {}'.format(key, result_proxy[key]))
+#
+#            cookie_id: 1
+#          cookie_name: chocolate chip
+#    cookie_recipe_url: http://some.aweso.me/cookie/recipe.html
+#           cookie_sku: CC01
+#             quantity: 132
+#            unit_cost: 0.50
