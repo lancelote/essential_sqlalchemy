146aa8bb38172573870f331db01e19644cf8f50b
Pavel Karateev <karateev.pavel@ya.ru>
2018-01-14 11:25:18 +0300

    Complete test list

--- src/part1/chapter4/test_app.py
+++ src/part1/chapter4/test_app.py
@@ -1,4 +1,5 @@
 import unittest
+from decimal import Decimal
 
 from src.part1.chapter4.app import get_orders_by_customer
 from src.part1.chapter4.db import dal, prepare_db
@@ -34,3 +35,37 @@ class TestApp(unittest.TestCase):
     def test_orders_by_blank_customer_name_not_shipped_details(self):
         results = get_orders_by_customer('', shipped=False, details=True)
         self.assertEqual(results, [])
+
+    def test_orders_by_customer(self):
+        expected_result = [('wlk001', 'cookiemon', '111-111-1111')]
+        results = get_orders_by_customer('cookiemon')
+        self.assertEqual(results, expected_result)
+
+    def test_order_by_customer_shipped_only(self):
+        results = get_orders_by_customer('cookiemon', True)
+        self.assertEqual(results, [])
+
+    def test_orders_by_customer_unshipped_only(self):
+        expected_results = [('wlk001', 'cookiemon', '111-111-1111')]
+        results = get_orders_by_customer('cookiemon', False)
+        self.assertEqual(results, expected_results)
+
+    def test_orders_by_customer_with_details(self):
+        expected_results = [
+            ('wlk001', 'cookiemon', '111-111-1111', 'dark chocolate chip', 2, Decimal('1.00')),
+            ('wlk001', 'cookiemon', '111-111-1111', 'oatmeal raisin', 12, Decimal('3.00'))
+        ]
+        results = get_orders_by_customer('cookiemon', details=True)
+        self.assertEqual(results, expected_results)
+
+    def test_orders_by_customer_shipped_only_with_details(self):
+        results = get_orders_by_customer('cookiemon', True, True)
+        self.assertEqual(results, [])
+
+    def test_orders_by_customer_unshipped_only_details(self):
+        expected_results = [
+            ('wlk001', 'cookiemon', '111-111-1111', 'dark chocolate chip', 2, Decimal('1.00')),
+            ('wlk001', 'cookiemon', '111-111-1111', 'oatmeal raisin', 12, Decimal('3.00'))
+        ]
+        results = get_orders_by_customer('cookiemon', shipped=False, details=True)
+        self.assertTrue(results, expected_results)
