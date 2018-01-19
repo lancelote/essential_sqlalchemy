5018cb0660d74cfcff5237ef882d977cba24bd4c
Pavel Karateev <karateev.pavel@ya.ru>
2018-01-19 19:48:57 +0300

    Table reflection example

---
+++ src/part1/chapter5/s34_table_reflection.py
@@ -0,0 +1,27 @@
+from sqlalchemy import MetaData, Table, create_engine, select
+
+metadata = MetaData()
+engine = create_engine('sqlite:///../../../dbs/Chinook_Sqlite.sqlite')
+
+artist = Table('Artist', metadata, autoload=True, autoload_with=engine)
+assert artist.columns.keys() == ['ArtistId', 'Name']
+
+selection = select([artist]).limit(10)
+first_10_records = engine.execute(selection).fetchall()
+assert first_10_records[0] == (1, 'AC/DC')
+
+album = Table('Album', metadata, autoload=True, autoload_with=engine)
+# metadata.tables['Album']
+#
+#    Table('Album',
+#        MetaData(bind=None),
+#        Column('AlbumId', INTEGER(), table=<Album>, primary_key=True, nullable=False),
+#        Column('Title', NVARCHAR(length=160), table=<Album>, nullable=False),
+#        Column('ArtistId', INTEGER(), ForeignKey('Artist.ArtistId'), table=<Album>, nullable=False), schema=None)
+#    )
+
+# album.foreign_keys
+#
+#    {ForeignKey('Artist.ArtistId')}
+
+assert str(artist.join(album)) == '"Artist" JOIN "Album" ON "Artist"."ArtistId" = "Album"."ArtistId"'
