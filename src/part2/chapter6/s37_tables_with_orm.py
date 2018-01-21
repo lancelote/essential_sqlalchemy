f3931956b1ec9cd78749e3edbd5eea56de17d2f4
Pavel Karateev <karateev.pavel@ya.ru>
2018-01-21 19:52:47 +0300

    Defining tables with ORM

---
+++ src/part2/chapter6/s37_tables_with_orm.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, Integer, Numeric, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Cookie(Base):
+    __tablename__ = 'cookies'
+
+    cookie_id = Column(Integer(), primary_key=True)
+    cookie_name = Column(String(50), index=True)
+    cookie_recipe_url = Column(String(255))
+    cookie_sku = Column(String(55))
+    quantity = Column(Integer())
+    unit_cost = Column(Numeric(12, 2))
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer(), primary_key=True)
+    username = Column(String(15), nullable=False, unique=True)
+    email_address = Column(String(255), nullable=False)
+    phone = Column(String(20), nullable=False)
+    password = Column(String(25), nullable=False)
+    created_on = Column(DateTime(), default=datetime.now)
+    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
