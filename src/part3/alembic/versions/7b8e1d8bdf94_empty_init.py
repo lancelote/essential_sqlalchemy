656927ff0e6141cab37c9b880f529a921c455acf
Pavel Karateev <karateev.pavel@ya.ru>
2018-03-24 18:35:28 +0300

    Base empty migration

---
+++ src/part3/alembic/versions/7b8e1d8bdf94_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 7b8e1d8bdf94
+Revises: 
+Create Date: 2018-03-24 18:32:55.740614
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '7b8e1d8bdf94'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
