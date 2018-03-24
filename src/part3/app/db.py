6ff71ca22179db5cf598b5e849730131f08ab810
Pavel Karateev <karateev.pavel@ya.ru>
2018-03-24 19:36:57 +0300

    Migration autogeneration

--- src/part3/app/db.py
+++ src/part3/app/db.py
@@ -1,6 +1,17 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///alembictest.db')
 
 Base = declarative_base()
+
+
+class Cookie(Base):
+    __tablename__ = 'cookies'
+
+    cookie_id = Column(Integer(), primary_key=True)
+    cookie_name = Column(String(50), index=True)
+    cookie_recipe_url = Column(String(255))
+    cookie_sku = Column(String(55))
+    quantity = Column(Integer())
+    unit_cost = Column(Numeric(12, 2))
